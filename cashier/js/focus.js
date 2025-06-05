ed5b6c12efd66a086403c0c54349f6226adaa2a1
gandalf <104222912+anon1jju@users.noreply.github.com>
2025-06-05 17:50:37 +0700

    Create focus.js

---
+++ cashier/js/focus.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Temukan elemen input search
+    const searchInput = document.getElementById('product-search');
+    
+    // Pastikan elemen ada, lalu fokuskan kursor ke dalam input
+    if (searchInput) {
+        searchInput.focus();
+    }
+});
